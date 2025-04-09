320bfc3901c4c12fd6e70fd5c5d15591230a9af3
Jayson Phillips <git@jay.phillz>
2025-04-09 16:55:06 -0700

    feat(DayOne)!: finish AOC 2015 Day 1, Part Two
    - renamed "process_input" to "part_one"
    - added part_two(input: binary) to solution file
    - converts binary to list, pipe to Enum.with_index, then pipe to Enum.reduce_while
    - reduces until we hit below 0 for the first time, and returns that index

--- lib/day_one/solution.ex
+++ lib/day_one/solution.ex
@@ -1,7 +1,8 @@
 defmodule Aoc2015.DayOne.Solution do
   require Logger
 
-  def process_input(aoc_input) do
+  @spec part_one(binary()) :: integer()
+  def part_one(aoc_input) do
     items = String.graphemes(aoc_input)
     Logger.info("input length: #{String.length(aoc_input)}")
 
@@ -13,10 +14,30 @@ defmodule Aoc2015.DayOne.Solution do
       end
     end)
 
-    # floor = for x <- items do`
-    #   if x == "(", do: santa + 1, else: santa - 1
-    # end
+    floor # return the final floor
+  end
 
-    floor
+  @spec part_two(binary()) :: integer()
+  def part_two(aoc_input) do
+    data = String.split(aoc_input, "", trim: true)
+    Logger.info("input length: #{data}")
+
+    result = data
+    |> Enum.with_index
+    |> Enum.reduce_while(0, fn({item, idx}, acc) ->
+      if acc > -1 do
+        {:cont,
+        if item == "(" do
+          acc + 1
+        else
+          acc - 1
+        end}
+      else
+        {:halt, idx}
+      end
+    end)
+
+   Logger.info("is the result an integer? #{is_integer(result)}")
+   result # return the index where we hit the basement
   end
 end
